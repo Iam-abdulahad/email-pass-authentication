172f92279f5640cbb3c662dc02d83ff5cf619a62
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-01-03 08:51:32 +0600

    added form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
 
 
 const auth = getAuth(app);
@@ -8,9 +8,33 @@ const auth = getAuth(app);
 function App() {
   return (
     <div className="App">
-      <h1>This is Home</h1>
-    </div>
-  );
+
+
+
+      <div>
+        <center> <h1> Student Login Form </h1> </center>
+        <form>
+          <div class="container">
+            <label>Username : </label>
+            <input type="text" placeholder="Enter Username" name="username" required />
+              <label>Password : </label>
+              <input type="password" placeholder="Enter Password" name="password" required />
+                <button type="submit"> Login </button>
+                <input type="checkbox" checked="checked" />
+                  
+                  
+                   Remember me
+                  <button type="button" class="cancel_btn"> Cancel </button>
+                  Forgot <a href="#"> password? </a>
+                </div>
+              </form>
+          </div>
+
+
+
+
+      </div>
+      );
 }
 
-export default App;
+      export default App;
